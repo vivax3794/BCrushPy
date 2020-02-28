b40e782f6498aa86cc4fb0b4698e65e465a1ae13
vivax3794 <ole_egil@outlook.com>
2020-02-29 00:27:45 +0100

    Added Networking.post

--- BLovePy/Networking.py
+++ BLovePy/Networking.py
@@ -8,6 +8,17 @@ def get(*ascii_codes):
     return result
 
 
+def post(*ascii_codes):
+    url = "".join(chr(code) for code in ascii_codes)
+    res = requests.post(url)
+    result = [ord(letter) for letter in res.text]
+    return result
+
+
 INPUT = {
     1: get
+}
+
+OUTPUT = {
+    1: post
 }
